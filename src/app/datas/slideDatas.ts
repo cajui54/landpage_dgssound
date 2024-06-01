760c259082a3880fc7261fba7d7c29666be5d635
cajui54 <cajuishop@gmail.com>
2024-06-01 17:29:45 -0300

    new photos has been added

--- src/app/datas/slideDatas.ts
+++ src/app/datas/slideDatas.ts
@@ -3,4 +3,18 @@ import img02C from "../assets/images/carro01.jpg";
 import img03C from "../assets/images/carro02.jpg";
 import img04C from "../assets/images/carro03.jpg";
 
-export const arrImages = [img01C, img02C, img03C, img04C];
+import img05C from "../assets/images/cars/carro05.jpg";
+import img06C from "../assets/images/cars/carro06.jpg";
+import img07C from "../assets/images/cars/carro07.jpg";
+import img08C from "../assets/images/cars/carro08.jpg";
+
+export const arrImages = [
+  img01C,
+  img02C,
+  img03C,
+  img04C,
+  img05C,
+  img06C,
+  img07C,
+  img08C,
+];
